a7f7b29f8833164f6a225b4554b77f1f5084c4fc
gqoitic <gqoitic@gmail.com>
2020-04-14 18:35:46 +0500

    first commit

---
+++ src/main/java/com/gqoitic/console_game/Application.java
@@ -0,0 +1,11 @@
+package com.gqoitic.console_game;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 10; i++)
+			System.out.println("it works");
+	}
+
+}
